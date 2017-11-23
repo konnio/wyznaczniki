af4d422b7e2b3378c2dcb705f866e81d41de7d38
konnio <konnio@gmail.com>
2017-11-23 10:42:49 +0100

    Dzialajacy kod

--- uklad_rownan.c
+++ uklad_rownan.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-#include <math.h>
 
 void wczytaj (double *a1, double *b1, double *c1, double *a2, double *b2, double *c2) //* oznacza ze zmienne moga byc edytowalne
-{
+{										      //  deklaracja adresu dla zmiennej double
 	puts("Podaj wspolczynnik a1: "); scanf("%lf", a1);  //%lf dla zmiennych double
 	puts("Podaj wspolczynnik b1: "); scanf("%lf", b1);  //%ld dla zmiennych long int
-	puts("Podaj wspolczynnik c1: "); scanf("%lf", c1);
+	puts("Podaj wspolczynnik c1: "); scanf("%lf", c1);  //bez & bo jest przy wywolani
 	puts("Podaj wspolczynnik a2: "); scanf("%lf", a2);
 	puts("Podaj wspolczynnik b2: "); scanf("%lf", b2);
 	puts("Podaj wspolczynnik c2: "); scanf("%lf", c2);
@@ -25,15 +24,14 @@ double wyznacznik_y (double a1, double a2, double c1, double c2) {
 
 
 int main () {
-// jezeli long int do %ld
-// jezeli double to %lf
 	double a1, b1, c1, a2, b2, c2;
 	double W, Wx, Wy, x, y;
-	wczytaj(&a1, &b1, &c1, &a2, &b2, &c2);
+	wczytaj(&a1, &b1, &c1, &a2, &b2, &c2); //& - przekazuje adres w pamieci gdzie dane ktore beda wprowadzane
 	W = wyznacznik_glowny (a1, b1, a2, b2);
 	Wx = wyznacznik_x (b1, b2, c1, c2);
 	Wy = wyznacznik_y (a1, a2, c1, c2);
-	x = Wx/W;
-	y = Wy/W;
+	x = Wx / W;
+	y = Wy / W;
+	printf ("x = %lf, y = %lf\n", x, y);
 	return 0;
 }
